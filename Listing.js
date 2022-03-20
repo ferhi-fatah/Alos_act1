5780cea5c7ffbc6aba6e68f0e3c69113b9b72d07
ferhi-fatah <101930870+ferhi-fatah@users.noreply.github.com>
2022-03-20 23:41:49 +0100

    Update Listing.js

--- Listing.js
+++ Listing.js
@@ -6,7 +6,7 @@ var req = unirest( "GET" , "http://localhost:3000/currencies?_limit=10" );
 
 // la fonction qui permet de filtrer les ressources qui commence par M .
 function filtrer_M(URL) {
-    URL = URL + "?$filter=startswith(currencies.name,'M')" ;
+    URL = URL + "?$filter=startswith(name,'M')" ;
     var req = unirest( "GET" , URL ) ;
 }
 
