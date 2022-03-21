b25522a082bb6f8e481b111e8e4785a44e8cbc67
asmaa-aa <62666792+asmaa-aa@users.noreply.github.com>
2022-03-21 13:09:18 +0100

    Update Listing.js

--- Listing.js
+++ Listing.js
@@ -1,19 +1,22 @@
 var unirest = require("unirest");
 
-// la 1ere question : afficher les 10 premiers enregistrements.
+// la 1ere question : afficher les 10 premiers enregistrements :
 var req = unirest( "GET" , "http://localhost:3000/currencies?_limit=10" );
 
 
-// la fonction qui permet de filtrer les ressources qui commence par M .
-function filtrer_M(URL) {
+// la fonction qui permet de filtrer les ressources qui commence par M :
+function filtrer_M( URL ) {
+    
     URL = URL + "?$filter=startswith(name,'M')" ;
     var req = unirest( "GET" , URL ) ;
 }
+
 
 req.headers({
         "cache-control": "no-cache",
     })
     
+
 req.end(function (res) {
         if (res.error) throw new Error(res.error)
         console.log(res.body)
